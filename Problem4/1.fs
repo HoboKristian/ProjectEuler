356a37a257c36d2659b5981d5043cdac2d3c4847
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2016-10-03 17:46:36 +0200

    Now uses list.rev instead of the custom method. Also reuses the same variable to compare regular and reversed

--- Problem4/1.fs
+++ Problem4/1.fs
@@ -1,42 +1,30 @@
-let nums f t =
+let numsSeq f t =
     seq {
         for x in f..t do
             for y in f..t do
                 yield (x*y) }
-    |> Seq.toList
 
-let rec reverse ls =
-    if List.length ls > 0 then
-        seq {
-            yield! reverse (List.tail ls)
-            yield List.head ls }
-    else
-        Seq.empty
-
-let r l =
-    reverse l
-    |> Seq.toList
-
-let rec spl s =
+let rec split s =
     let n = s / 10
     if n = 0 then
         seq {
             yield (s - n * 10) }
     else
         seq {
-            yield! spl n
+            yield! split n
             yield (s - n * 10) }
 
-let split s =
-    spl s
-    |> Seq.toList
-
 let palindromes f t=
-    nums f t
-    |> List.filter (fun x -> (split x) = (r (split x)))
+    numsSeq f t
+    |> Seq.filter
+        (fun x ->
+            let reg = Seq.toList (split x)
+            let reversed = List.rev reg
+            reg = reversed )
 
 let result =
     palindromes 101 999
+    |> Seq.toList
     |> List.max
 
 printfn "%A" result
