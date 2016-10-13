1e2230825d88d66438da3019c7457e4d2f56c7b0
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2016-10-13 03:10:03 +0200

    Solved problem 7

---
+++ Problem7/1.fs
@@ -0,0 +1,11 @@
+let odds n =
+    seq {2..n-1}
+
+let isPrime n =
+    Seq.forall(fun x -> n % x <> 0) (odds n)
+
+let l =
+    Seq.initInfinite(fun index -> index + 2)
+    |> Seq.filter(fun n -> (isPrime n))
+
+printfn "%A" (Seq.last (Seq.take 10001 l))
