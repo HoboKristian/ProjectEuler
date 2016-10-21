da514683d76ca936020f84dc1c08293dc191b076
Kristian Flatheim Jensen <kristianflatheim@hotmail.com>
2016-10-21 11:26:11 +0200

    Changed up and down into one function and moved the odd/even logic to this function

--- Problem14/1.fs
+++ Problem14/1.fs
@@ -1,20 +1,14 @@
-let up n=
-    n / 2L
-
-let down n=
-    n*3L + 1L
+let nextNumber n = if n % 2L = 0L then n / 2L else n*3L + 1L
 
 let rec iterate n=
-    if n = 1L then
-        seq {yield n}
-    elif n % 2L = 0L then
-        seq {yield n
-             yield! iterate (up n)}
-    else
-        seq {yield n
-             yield! iterate (down n)}
+    seq {
+        if n = 1L then
+            yield n
+        else
+            yield n
+            yield! iterate (nextNumber n) }
 
-let nums = [1L..10000L]
+let nums = [1L..100L]
 let m =
     nums
     |> Seq.maxBy (fun index -> Seq.length (iterate index))
